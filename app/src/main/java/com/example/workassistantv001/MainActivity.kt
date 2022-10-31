884e9ef25e8019c25478039e837b19c20cfd4774
maronas <41578223+maronas@users.noreply.github.com>
2022-10-31 07:10:16 +0200

    Work Assistant, activities move

--- app/src/main/java/com/example/workassistantv001/MainActivity.kt
+++ app/src/main/java/com/example/workassistantv001/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.workassistantv001
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_manage_objects =findViewById(R.id.btnManageObjects) as Button
+        val btn_manage_workers = findViewById(R.id.btnManageWorkers) as Button
+
+        btn_manage_objects.setOnClickListener {
+            val intent = Intent(this, ObjectsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_manage_workers.setOnClickListener {
+            val intent = Intent(this, WorkersActivity::class.java)
+            startActivity(intent)
+        }
     }
-//    comment
 }
---
+++ app/src/main/java/com/example/workassistantv001/ObjectsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.workassistantv001
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ObjectsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_objects)
+    }
+}
