9903b540905342a218a94e2e128fef71caaaa282
maronas <maronas@yandex.ru>
2023-02-09 18:48:32 +0200

    Added spinner on main

--- app/src/main/java/com/example/workassistantv001/MainActivity.kt
+++ app/src/main/java/com/example/workassistantv001/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.workassistantv001
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.icu.util.LocaleData
@@ -7,10 +8,9 @@ import android.os.Binder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.InputBinding
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.workassistantv001.databinding.ActivityMainBinding
@@ -19,13 +19,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.rvWorkers
 import kotlinx.android.synthetic.main.activity_objects.*
 import kotlinx.android.synthetic.main.activity_workers.*
+import kotlinx.android.synthetic.main.day_worker_view.*
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var mainAdapter: MainAdapter
     private lateinit var txt_selected_date : TextView
     private lateinit var database1 : DatabaseReference
@@ -34,19 +35,33 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var objectArrayList: ArrayList<Object>
     private lateinit var workerArrayList: ArrayList<Worker>
+
+    @SuppressLint("MissingInflatedId")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        // -------------------- Creating fucking spinner
+//        val spinner: Spinner = findViewById(R.id.spObjects)
+//        var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
+//
+//        ArrayAdapter(
+//            baseContext,
+//            android.R.layout.simple_spinner_item,
+//            languages
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            spinner.adapter = adapter
+//        }
+        // -------------------- End of fucking spinner
         val btn_manage_objects = findViewById(R.id.btnManageObjects) as Button
         val btn_manage_workers = findViewById(R.id.btnManageWorkers) as Button
         val btn_select_date = findViewById(R.id.btnSelectDate) as Button
         txt_selected_date = findViewById(R.id.tvSelectedDate)
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         txt_selected_date.text = LocalDateTime.now().format(formatter)
-
 
         val myCalendar = Calendar.getInstance()
         val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
@@ -73,9 +88,7 @@ class MainActivity : AppCompatActivity() {
             DatePickerDialog(this, datePicker,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
-
         mainAdapter = MainAdapter(mutableListOf(), mutableListOf())
-
         rvWorkers.adapter = mainAdapter
         rvWorkers.layoutManager = LinearLayoutManager(this)
 
@@ -105,6 +118,7 @@ class MainActivity : AppCompatActivity() {
                     for (workerSnapshot in snapshot.children) {
                         val worker = workerSnapshot.getValue(Worker::class.java)
                         workerArrayList.add(worker!!)
+
                     }
                     rvWorkers.adapter = MainAdapter(objectArrayList, workerArrayList)
                 }
@@ -140,9 +154,12 @@ class MainActivity : AppCompatActivity() {
         txt_selected_date.setText(sdf.format(myCalendar.time))
     }
 
-    private fun refresh(){
-        mainAdapter.notifyDataSetChanged()
-        workerArrayList.clear()
-        objectArrayList.clear()
+    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        Toast.makeText(this, "Selected : ", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
+    }
+
 }
--- app/src/main/java/com/example/workassistantv001/MainAdapter.kt
+++ app/src/main/java/com/example/workassistantv001/MainAdapter.kt
@@ -4,6 +4,8 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.day_worker_view.view.*
@@ -13,12 +15,13 @@ import kotlinx.android.synthetic.main.worker_view.view.*
 
 class MainAdapter(
     private val objects: MutableList<Object>,
-    private val workers: MutableList<Worker>
+    private val workers: MutableList<Worker>,
 ): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
+    var languages = arrayOf("Java", "PHP", "Kotlin", "Javascript", "Python", "Swift")
 
     class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+
         return MainViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.day_worker_view,
@@ -31,9 +34,20 @@ class MainAdapter(
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val curWorker = workers[position]
+
         holder.itemView.apply {
             tvWorkerName.text = curWorker.name
+            ArrayAdapter(
+                context,
+                android.R.layout.simple_spinner_item,
+                languages
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spObjects.adapter = adapter
+            }
         }
+
+
     }
 
     override fun getItemCount(): Int{
